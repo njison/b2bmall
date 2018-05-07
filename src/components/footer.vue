cfdc2eb634d5148ad8dcd81bbcc2f458f75e6724
njison <njison@126.com>
2018-05-07 17:37:15 +0800

    test footer
    测试组件引用

---
+++ src/components/footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>footer</h2>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    methods: {
+
+    },
+    components: {
+    }
+  }
+</script>
